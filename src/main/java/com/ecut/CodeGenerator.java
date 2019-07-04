0a92a933a924c680b7ff62b1482c5d07cfcb4cf1
Amy <aimeizheng@outlook.com>
2019-07-04 16:43:02 +0800

    test

---
+++ src/main/java/com/ecut/CodeGenerator.java
@@ -0,0 +1,16 @@
+package com.ecut;
+
+import org.jooq.codegen.GenerationTool;
+
+/**
+ * @author Amy
+ * @date 2019-07-04 16:27
+ * @description:
+ */
+
+public class CodeGenerator {
+
+    public static void main(String[] args) throws Exception {
+        GenerationTool.main(new String[]{"/Users/zhengamy/IdeaProjects/jooq-test/src/main/resources/library.xml"});
+    }
+}
